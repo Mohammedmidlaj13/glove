38d7c3e8c64bdb46f355f18547cfef25ef5bbea5
Mohammedmidlaj13 <53626850+Mohammedmidlaj13@users.noreply.github.com>
2021-07-02 13:34:59 +0530

    Update constants.dart

--- constants.dart
+++ constants.dart
@@ -38,18 +38,33 @@ TextStyle profileName = TextStyle(
     fontWeight: FontWeight.w100,
     fontFamily: 'Staatliches'
 );
+
 TextStyle settingsMain = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w100,
     fontFamily: 'Staatliches',
     color: CupertinoColors.white
 );
+
 TextStyle loginButton = TextStyle(
     fontSize: 25,
     fontWeight: FontWeight.w200,
     fontFamily: 'Staatliches'
 );
 
+TextStyle customTag = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w100,
+    fontFamily: 'Staatliches',
+    color: Colors.white54,
+    decoration: TextDecoration.none,
+);
+TextStyle comboTag = TextStyle(
+    fontSize: 21,
+    fontFamily: 'Staatliches',
+    color: Colors.white,
+    decoration: TextDecoration.none,
+);
 
 
 Color mainColor=Color(0xff17181E);
