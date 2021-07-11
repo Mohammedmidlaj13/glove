b43da20784c8a77fdc6b3a9de09fcfffc054cfcd
Mohammed Midlaj <53626850+Mohammedmidlaj13@users.noreply.github.com>
2021-07-11 14:30:44 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,30 +1,70 @@
-import 'mainscreen.dart';
+import 'package:mevoicingglove/screens/predefinedoutputscreen.dart';
+import 'package:mevoicingglove/screens/usermanual.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/mainscreen.dart';
 import 'package:flutter/material.dart';
-import 'loginpage.dart';
-import 'settingspage.dart';
-import 'forgotpasspage.dart';
-import 'registerpage.dart';
-import 'outputscreen.dart';
-import 'customisationpage.dart';
+import 'screens/loginpage.dart';
+import 'screens/settingspage.dart';
+import 'screens/forgotpasspage.dart';
+import 'screens/registerpage.dart';
+import 'screens/outputscreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'authentication/authentication_services.dart';
+import 'package:mevoicingglove/screens/customisationpage.dart';
+import 'screens/usermanual.dart';
+import 'screens/usermanual2.dart';
+//Code
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
 
-void main() {
-  runApp(
-      MaterialApp(
+  @override
+  Widget build(BuildContext context) {
+    //Code
+    return MultiProvider(providers: [
+      Provider<AuthenticationService>(
+        create: (_) => AuthenticationService(FirebaseAuth.instance),
+      ),
+      StreamProvider(
+        create: (context) => context.read<AuthenticationService>().authStateChanges,
+      ),
+    ],
+      child: MaterialApp(
         routes: {
-          '/':(context)=>MainScreen(),
-          '/loginpage':(context)=>LoginPage(),
-          '/settingspage':(context)=>SettingsPage(),
-          '/forgotpasspage':(context)=>ForgotPasswordPage(),
-          '/registerpage':(context)=>RegisterPage(),
-          '/outputscreen':(context)=>OutputScreen(),
+          '/main':(context)=>MainScreen(),
+           '/loginpage':(context)=>LoginPage(),
+           '/settingspage':(context)=>SettingsPage(),
+           '/forgotpasspage':(context)=>ForgotPasswordPage(),
+           '/registerpage':(context)=>RegisterPage(),
+           '/outputscreen':(context)=>Predefined_OutputScreen(),
           '/customisationpage':(context)=>CustomisationPage(),
+          '/usermanual1':(context)=>UserManual(),
+          '/usermanual2':(context)=>UserManual2(),
 
         },
-        initialRoute: '/',
+        initialRoute: '/main',
         theme: ThemeData.dark().copyWith(
             scaffoldBackgroundColor: Colors.black
         ),
-      )
-  );
+        home: AuthenticationWrapper(),
+      ),
+    );
+  }
+}
+
+//Code
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+    if(firebaseUser != null) {
+      return MainScreen();
+    }
+    return RegisterPage();
+  }
 }
