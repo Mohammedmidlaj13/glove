a0fac85df64f9bd38a4bb995b1055a4c44b6a918
Mohammed Midlaj <53626850+Mohammedmidlaj13@users.noreply.github.com>
2021-07-13 23:54:02 +0530

    Update predefinedoutputscreen.dart

--- predefinedoutputscreen.dart
+++ predefinedoutputscreen.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -14,15 +16,22 @@ class Predefined_OutputScreen extends StatefulWidget {
 class _Predefined_OutputScreenState extends State<Predefined_OutputScreen> {
   //Flutter TTS package
   final FlutterTts flutterTts = FlutterTts();
+
   //Realtime Databasse
   final fb = FirebaseDatabase.instance;
   var retrievedName = "";
+  // var outputString="";
   String inputvalue = "";
+  String gest01 = "gest01";
+  String gest02 = "gest02";
+  String outputName = "Hello";
+
   Future _speak() async {
     await flutterTts.setLanguage("en-IN");
     flutterTts.setPitch(1);
     await flutterTts.speak(retrievedName);
   }
+
   @override
   Widget build(BuildContext context) {
 
@@ -63,7 +72,6 @@ class _Predefined_OutputScreenState extends State<Predefined_OutputScreen> {
                   children: [
                     Hero(
                       tag: 'logo',
-
                       child: GestureDetector(
                         onTap: _fetchGesture,
                         child: Container(child:  Image.asset('images/wifi.png',height:height*0.2 ,width: width*0.2,),),
@@ -86,9 +94,52 @@ class _Predefined_OutputScreenState extends State<Predefined_OutputScreen> {
       ref.child("inputvalue").once().then((DataSnapshot data) {
         setState(() {
           retrievedName = data.value;
+         //retrievedName='gest07';
+          _checkGesture();
           _speak();
         });
       });
     });
+  }
+  _checkGesture() {
+    bool flag=false;
+    List<String>outputStrings=[
+      'Hello! How are you doing?',
+      'Thanks a lot',
+      'Can I get something to eat or drink',
+      'That\'s great',
+      'Yes',
+      'No',
+      'What time is it?',
+      'Excuse me',
+      'Can you please help me',
+      'How much does this cost?',
+      'Goodbye!',
+    ];
+    for(int i=1; i<=9;i++)
+    {
+      if(retrievedName == 'gest0$i') {
+        setState(() {
+          flag = true;
+          retrievedName = outputStrings[i-1];
+        }
+        );
+        break;
+      }
+    }
+    if(flag==false)
+    {
+      setState(() {
+        flag = true;
+        retrievedName = 'Customised code';
+
+      }
+      );
+
+      //write the customised ka code here
+
+    }
+
+
   }
 }
